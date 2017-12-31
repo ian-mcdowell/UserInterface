8c57c44fddc5954cc0c1731fa2dce0374e749b88
Ian McDowell <me@ianmcdowell.net>
2017-12-30 21:10:26 -0800

    Switched from CMake to Xcode & ibuild

---
+++ Framework/UserInterface.h
@@ -0,0 +1,19 @@
+//
+//  UserInterface.h
+//  UserInterface
+//
+//  Created by Ian McDowell on 12/30/17.
+//  Copyright © 2017 Ian McDowell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UserInterface.
+FOUNDATION_EXPORT double UserInterfaceVersionNumber;
+
+//! Project version string for UserInterface.
+FOUNDATION_EXPORT const unsigned char UserInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserInterface/PublicHeader.h>
+
+
---
+++ Sources/Imported/SOTiltedTabViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SOTiltedTabViewController.swift
+//  UserInterface
+//
+//  Created by Ian McDowell on 12/30/17.
+//  Copyright © 2017 Ian McDowell. All rights reserved.
+//
+
+import TiltedTabView
+
+open class SOTiltedTabViewController: TiltedTabViewController {
+    // Subclass to export from staticlib as part of this module
+}
